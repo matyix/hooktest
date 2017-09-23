555b9914ca04308783f1f044426fa598524c71be
Janos Matyas <janos.matyas@gmail.com>
2017-09-23 13:16:44 +0200

    Update entrypoint.sh

--- ngrok/entrypoint.sh
+++ ngrok/entrypoint.sh
@@ -4,18 +4,6 @@ if [ -n "$@" ]; then
   exec "$@"
 fi
 
-# Legacy compatible:
-if [ -z "$NGROK_PORT" ]; then
-  if [ -n "$HTTPS_PORT" ]; then
-    NGROK_PORT="$HTTPS_PORT"
-  elif [ -n "$HTTPS_PORT" ]; then
-    NGROK_PORT="$HTTP_PORT"
-  elif [ -n "$APP_PORT" ]; then
-    NGROK_PORT="$APP_PORT"
-  fi
-fi
-
-
 ARGS="ngrok"
 
 # Set the protocol.
@@ -24,23 +12,6 @@ if [ "$NGROK_PROTOCOL" = "TCP" ]; then
 else
   ARGS="$ARGS http"
   NGROK_PORT="${NGROK_PORT:-80}"
-fi
-
-# Set the authorization token.
-if [ -n "$NGROK_AUTH" ]; then
-  ARGS="$ARGS -authtoken=$NGROK_AUTH "
-fi
-
-# Set the subdomain or hostname, depending on which is set
-if [ -n "$NGROK_HOSTNAME" ] && [ -n "$NGROK_AUTH" ]; then
-  ARGS="$ARGS -hostname=$NGROK_HOSTNAME "
-elif [ -n "$NGROK_SUBDOMAIN" ] && [ -n "$NGROK_AUTH" ]; then
-  ARGS="$ARGS -subdomain=$NGROK_SUBDOMAIN "
-elif [ -n "$NGROK_HOSTNAME" ] || [ -n "$NGROK_SUBDOMAIN" ]; then
-  if [ -z "$NGROK_AUTH" ]; then
-    echo "You must specify an authentication token after registering at https://ngrok.com to use custom domains."
-    exit 1
-  fi
 fi
 
 # Set a custom region
